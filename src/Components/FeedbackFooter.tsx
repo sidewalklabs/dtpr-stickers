82dbd55ce3633adc63c55e408aa82c74a1049fe0
Ananta Pandey <pandananta@gmail.com>
2019-07-16 21:49:41 -0400

    privacy update: make it transparent that feedback widget is a mailto link

--- src/Components/FeedbackFooter.tsx
+++ src/Components/FeedbackFooter.tsx
@@ -13,10 +13,20 @@ import ReactGA from "react-ga";
 ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_KEY || "");
 ReactGA.set({ anonymizeIp: true });
 
+const COLOR_POSITIVE = "#E5F6D2"
+const COLOR_CRITICAL = "#F5CCDD"
+const COLOR_NEUTRAL = "#FDEDD3"
+
+type FeedbackType = 'positive' | 'critical' | 'neutral'
+
 interface Props extends WithStyles<typeof styles> {
   placeName?: string;
   technology: string;
   email: string;
+}
+
+interface State {
+  activeFeedbackButton: FeedbackType | ''
 }
 
 const styles = (theme: Theme) =>
@@ -32,65 +42,21 @@ const styles = (theme: Theme) =>
       display: "flex",
       flexDirection: "column"
     },
-
     feedbackTitle: {
       fontSize: "14px",
       fontWeight: "bold",
       margin: "16px 0"
     },
-    dtprFeedbackButton: {
-      padding: "12px 24px"
+    dtprFeedbackButtons: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      padding: '20px',
     },
-    feedbackNegative: {
-      backgroundColor: "#F5CCDD",
+    dtprFeedbackButton: {
       border: "1px solid rgba(0, 0, 0, 0.1)",
       borderRadius: "24px",
-      overflow: "visible",
-      float: "left",
-      marginLeft: "20px",
-      "&:hover": {
-        backgroundColor: "#F5CCDD",
-        border: "1px solid rgba(0, 0, 0, 0.1)"
-      },
-      "&:active": {
-        border: "1px solid rgba(0, 0, 0, 0.1)"
-      },
-      "&:focus": {
-        border: "1px solid rgba(0, 0, 0, 0.1)"
-      }
-    },
-    feedbackMeh: {
-      backgroundColor: "#FDEDD3",
-      border: "0.5px solid rgba(0, 0, 0, 0.1)",
-      borderRadius: "24px",
-      overflow: "visible",
-      "&:hover": {
-        backgroundColor: "#FDEDD3",
-        border: "1px solid rgba(0, 0, 0, 0.1)"
-      },
-      "&:active": {
-        border: "1px solid rgba(0, 0, 0, 0.1)"
-      },
-      "&:focus": {
-        border: "1px solid rgba(0, 0, 0, 0.1)"
-      }
-    },
-    feedbackPositive: {
-      backgroundColor: "#E5F6D2",
-      border: "0.5px solid rgba(0, 0, 0, 0.1)",
-      borderRadius: "24px",
-      overflow: "visible",
-      float: "right",
-      marginRight: "20px",
-      "&:hover": {
-        backgroundColor: "#E5F6D2",
-        border: "1px solid rgba(0, 0, 0, 0.1)"
-      },
-      "&:active": {
-        border: "1px solid rgba(0, 0, 0, 0.1)"
-      },
-      "&:focus": {
-        border: "1px solid rgba(0, 0, 0, 0.1)"
+      '&:hover': {
+        border: "1px solid rgba(0, 0, 0, 0.1)",
       }
     },
     feedbackSendButton: {
@@ -106,60 +72,90 @@ const styles = (theme: Theme) =>
       color: "rgba(0,0,0,.7)",
       textIndent: 0,
       margin: "20px 20px",
-      display: "block"
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center'
     },
     sendButtonIcon: {
-      float: "right",
-      marginTop: "8px",
       height: "20px",
       width: "20px"
     }
   });
 
-class FeedbackFooter extends Component<Props, any> {
-  mailtoFeedback(subject: string) {
-    const { placeName, email, technology } = this.props
-    const feedbackSubject = `Some ${subject} feedback for you about ${placeName}`
-    const feedbackBody = `Hello, \n I'd like to share my thoughts on ${technology} ...`
-    const mailUrl = `mailto:${email}?subject=${feedbackSubject}&body=${feedbackBody}`
+class FeedbackFooter extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      activeFeedbackButton: '',
+    };
+  }
+
+  onFeedbackButtonClick(feedbackButton: FeedbackType) {
+    const { technology } = this.props
+    this.setState({ activeFeedbackButton: feedbackButton })
 
     ReactGA.event({
       category: 'User',
-      action: `Clicked Feedback: ${subject}`,
+      action: `Clicked Feedback: ${feedbackButton}`,
       label: technology
     });
-    window.location.href = mailUrl;
   }
+
+  getHref() {
+    const { placeName, email, technology } = this.props
+    const { activeFeedbackButton } = this.state
+    const feedbackSubject = `Some ${activeFeedbackButton} feedback for you about ${placeName}`
+    const feedbackBody = `Hello, \n I'd like to share my thoughts on ${technology} ...`
+    const mailUrl = `mailto:${email}?subject=${feedbackSubject}&body=${feedbackBody}`
+    return mailUrl
+  }
+
+
   render() {
     const { classes } = this.props;
+    const { activeFeedbackButton } = this.state
+
     return (
       <div className={classes.dtprFeedbackContainer}>
         <div className={classes.dtprFeedbackBox}>
           <Typography className={classes.feedbackTitle}>
             How do you feel about this technology?
           </Typography>
-          <div>
+          <div className={classes.dtprFeedbackButtons}>
             <Button
               variant="outlined"
-              color="primary"
-              className={classes.feedbackNegative}
-              onClick={() => this.mailtoFeedback("critical")}
+              disableRipple
+              className={classes.dtprFeedbackButton}
+              style={{
+                backgroundColor: COLOR_CRITICAL,
+                opacity: activeFeedbackButton === 'critical' ? 1 : 0.5
+              }}
+              onClick={() => this.onFeedbackButtonClick("critical")}
             >
               <img src={"/images/feedback/feedback_sad.svg"} />
             </Button>
             <Button
               variant="outlined"
-              color="primary"
-              className={classes.feedbackMeh}
-              onClick={() => this.mailtoFeedback("neutral")}
+              disableRipple
+              className={classes.dtprFeedbackButton}
+              style={{
+                backgroundColor: COLOR_NEUTRAL,
+                opacity: activeFeedbackButton === 'neutral' ? 1 : 0.5
+              }}
+              onClick={() => this.onFeedbackButtonClick("neutral")}
             >
               <img src={"/images/feedback/feedback_meh.svg"} />
             </Button>
             <Button
               variant="outlined"
-              color="primary"
-              className={classes.feedbackPositive}
-              onClick={() => this.mailtoFeedback("positive")}
+              disableRipple
+              className={classes.dtprFeedbackButton}
+              style={{
+                backgroundColor: COLOR_POSITIVE,
+                opacity: activeFeedbackButton === 'positive' ? 1 : 0.5
+              }}
+              onClick={() => this.onFeedbackButtonClick("positive")}
             >
               <img src={"/images/feedback/feedback_happy.svg"} />
             </Button>
@@ -168,9 +164,9 @@ class FeedbackFooter extends Component<Props, any> {
             variant="outlined"
             color="primary"
             className={classes.feedbackSendButton}
-            onClick={() => this.mailtoFeedback("general")}
+            href={this.getHref()}
           >
-            Say more about why…
+            Email us your thoughts ...
             <SendIcon className={classes.sendButtonIcon} />
           </Button>
         </div>
