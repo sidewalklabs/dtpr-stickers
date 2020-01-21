b10bb5e242e9e32d85015969a7f7b5b3b9b59bb1
Ananta Pandey <pandananta@gmail.com>
2020-01-21 15:50:42 -0500

    remove sidewalklabs user domain restriction

--- src/Components/Authentication.tsx
+++ src/Components/Authentication.tsx
@@ -13,8 +13,9 @@ const uiConfig = {
     {
       provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       customParameters: {
+        // TODO: implement whitelist
         // restrict to display of swl emails only
-        hd: 'sidewalklabs.com',
+        // hd: 'sidewalklabs.com',
         // Forces account selection even when one account
         // is available.
         prompt: 'select_account',
