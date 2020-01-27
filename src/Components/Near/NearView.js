cb2eb071a807d051b86a3d9f383c17b4ee27b017
Scott Wright <scott@normative.com>
2020-01-27 15:35:57 -0500

    Set map width and height to 100% of container

--- src/Components/Near/NearView.js
+++ src/Components/Near/NearView.js
@@ -33,8 +33,8 @@ class NearView extends Component {
     super(props);
     this.state = {
       viewport: {
-        width: 800,
-        height: 800,
+        width: '100%',
+        height: '100%',
         latitude: 37.785164,
         longitude: -100,
         zoom: 3.5,
